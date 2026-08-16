1705cd325469883c4bbd765fcd055faead8a781e
SIALOU <sialoutibanalarissa@gmail.com>
2026-08-16 19:32:06 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -74,11 +74,11 @@ Passionnée par le développement web et mobile, je conçois des applications et
 </p>
 
 <p>
-  J'ai ainsi intégré Eranove Academy en 2024, où j'ai suivi une formation
-  professionnalisante en Web Mobile Business Intelligence. Cette expérience
-  m'a permis de développer mes compétences en développement web et mobile,
-  bases de données et conception de solutions numériques. J'ai obtenu en
-  avril 2026 ma Licence Professionnelle avec la <strong>Mention Très Bien</strong>.
+  J'ai ainsi intégré Eranove Academy en décembre 2024, où j'ai suivi une formation
+  professionnalisante en Web Mobile Business Intelligence d'une durée de 1 an 4 mois . Cette formation
+  m'a permis de développer mes compétences en développement web et mobile,en gestion et conception
+  bases de données, ainsi qu'en conception de solutions numériques. À l’issue de ce parcours, j’ai obtenu en avril 2026 ma Licence Professionnelle avec la  <strong>Mention Très Bien</strong>.
+  une reconnaissance qui vient valoriser mon engagement, ma persévérance et les compétences acquises au cours de ma formation.
 </p>
 
 <p>
